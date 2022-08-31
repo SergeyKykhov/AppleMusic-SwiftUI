5027a84b07e488b067814b20752ab8ef3ccd99e7
Sergey Kykhov <ghrle@mail.ru>
2022-08-31 21:58:28 +0300

    Initial Commit

---
+++ AppleMusic SwiftUI/AppleMusic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_SwiftUIApp.swift
+//  AppleMusic SwiftUI
+//
+//  Created by Sergey Kykhov on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
